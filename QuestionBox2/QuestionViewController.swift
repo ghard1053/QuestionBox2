5ba838757c7745670ddb2f19048ec09afaaa9d7c
Takao <>
2018-05-12 16:51:19 +0900

    get image

--- QuestionBox2/QuestionViewController.swift
+++ QuestionBox2/QuestionViewController.swift
@@ -7,29 +7,90 @@
 //
 
 import UIKit
+import Firebase
+import TwitterKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITextViewDelegate, TWTRComposerViewControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  var username = String()
+  
+  @IBOutlet weak var questionTextView: UITextView!
+  @IBOutlet weak var answerTextView: UITextView!
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  var postImageView = UIImageView()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+    answerTextView.delegate = self
+    answerTextView.layer.borderWidth = 10
+    answerTextView.layer.borderColor = UIColor.blue.cgColor
+    answerTextView.textContainerInset = UIEdgeInsetsMake(20.0, 10.0, 10.0, 10.0)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    questionTextView.delegate = self
+    questionTextView.layer.borderWidth = 10
+    questionTextView.layer.borderColor = UIColor.blue.cgColor
+    questionTextView.textContainerInset = UIEdgeInsetsMake(20.0, 10.0, 10.0, 10.0)
     
+    titleLabel.text = username
+    
+    //ユーザーのnameをfirebaseから取ってくる
+    readUserNames()
+    
+    
+  }
 
-    /*
-    // MARK: - Navigation
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    answerTextView.resignFirstResponder()
+  }
+  
+  func getScreenShot() {
+    let rect = questionTextView.bounds
+    UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+    let context = UIGraphicsGetCurrentContext()!
+    questionTextView.layer.render(in: context)
+    let caputureImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    postImageView.image = caputureImage
+  }
+  
+  @IBAction func reload(_ sender: Any) {
+    readUserNames()
+  }
+  
+  @IBAction func postTwitter(_ sender: Any) {
+    getScreenShot()
+    
+    let composer = TWTRComposer()
+    composer.setText(answerTextView.text)
+    composer.setImage(postImageView.image)
+    composer.setURL(URL(string: "https://critiq.jp/"))
+    composer.show(from: self, completion: nil)
+  }
+  
+  
+  func readUserNames() {
+    let db = Firestore.firestore()
+    db.collection("UserName").whereField("name", isEqualTo: username).getDocuments { (snapshot, error) in
+      if error != nil {
+        
+      } else {
+        for document in (snapshot?.documents)! {
+          if let question = document.data()["question"] as? String {
+            self.questionTextView.text = question
+            if let font = document.data()["font"] as? CGFloat {
+              self.questionTextView.font = UIFont.systemFont(ofSize: font)
+            }
+          }
+        }
+      }
     }
-    */
+  }
 
 }
--- QuestionBox2/ViewController.swift
+++ QuestionBox2/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import TwitterKit
+import Firebase
 
 class ViewController: UIViewController {
 
+  var username = String()
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,40 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func login(_ sender: Any) {
+    TWTRTwitter.sharedInstance().logIn { (session, error) in
+      if let sess = session {
+        self.username = sess.userName
+      }
+      
+      let okorNot = UserDefaults.standard.string(forKey: "OK")
+      if okorNot == nil {
+        //Firebaseにusernameを飛ばす
+        self.postUserName()
+        UserDefaults.standard.set("OK", forKey: "OK")
+
+      } else {
+        
+      }
+      
+      self.performSegue(withIdentifier: "next", sender: nil)
+      
+    }
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let questionVC = segue.destination as! QuestionViewController
+    questionVC.username = self.username
+  }
+  
+  func postUserName() {
+    let username = self.username
+    let params: [String:Any] = ["name": username, "question": ""]
+    
+    let userRef = Firestore.firestore()
+    userRef.collection("UserName").document(username).setData(params)
+  }
+  
 
 }
 
